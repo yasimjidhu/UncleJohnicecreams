6fe930884a9f3908251047ec2c6a22f845defdbc
yasimjidhu <139307040+yasimjidhu@users.noreply.github.com>
2023-08-27 16:15:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function btnclick(){
+    var a=document.getElementById("toggle1")
+    if(a.style.display=="none")
+    {
+        a.style.display="block"
+    }
+    else
+    {
+        a.style.display="none"
+    }
+
+}
